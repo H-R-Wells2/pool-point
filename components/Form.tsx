65fbe16481a1c3202e2bceb8d7a3769052d6c780
h-r-wells2 <hrwells10246@gmail.com>
2023-09-18 23:36:47 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -5,17 +5,36 @@ interface FormProps {
 }
 
 const Form: React.FC<FormProps> = ({ onSubmit }) => {
-  const [names, setNames] = useState(["", "", "", ""]);
+  const [names, setNames] = useState<string[]>(["", "", "", ""]);
 
   const handleChange = (index: number, value: string) => {
-    const newNames = [...names];
-    newNames[index] = value;
-    setNames(newNames);
+    setNames((prevNames) => {
+      const newNames = [...prevNames];
+      newNames[index] = value;
+      return newNames;
+    });
   };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     onSubmit(names);
+  };
+
+  const renderInputs = () => {
+    return names.map((name, index) => (
+      <input
+        key={index}
+        type="text"
+        value={name}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          handleChange(index, e.target.value)
+        }
+        placeholder={`Player ${index + 1}`}
+        className="mb-2 p-2 rounded-lg outline-none text-black"
+        required
+        minLength={3}
+      />
+    ));
   };
 
   return (
@@ -24,20 +43,7 @@ const Form: React.FC<FormProps> = ({ onSubmit }) => {
         className="flex flex-col justify-center items-center w-fit bg-gray-700 px-3 py-5 rounded-lg"
         onSubmit={handleSubmit}
       >
-        {names.map((name, index) => (
-          <input
-            key={index}
-            type="text"
-            value={name}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              handleChange(index, e.target.value)
-            }
-            placeholder={`Player ${index + 1}`}
-            className="mb-2 p-2 rounded-lg outline-none text-black"
-            required
-            minLength={3}
-          />
-        ))}
+        {renderInputs()}
         <button
           type="submit"
           className="bg-teal-500 text-white px-4 py-2 rounded-lg"

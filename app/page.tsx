c2724d9692e65ca52b97c4be88c5ae31fca2848b
h-r-wells2 <hrwells10246@gmail.com>
2023-09-20 18:03:19 +0530

    added alert on submit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import PlayerCard from "../components/PlayerCard";
 import Form from "../components/Form";
 import ResultCard from "@/components/ResultCard";
 import { createResult } from "@/lib/actions/result.actions";
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
 
 const Home = () => {
   const [playerNames, setPlayerNames] = useState<string[]>([]);
@@ -57,14 +58,42 @@ const Home = () => {
                 onClick={toggleResultCard}
                 className="mt-3 bg-teal-500 text-white px-4 py-2 rounded-lg"
               >
-                Show Results
+                Show Result
               </button>
-              <button
-                onClick={submitResult}
-                className="mt-3 bg-teal-500 text-white px-4 py-2 rounded-lg"
-              >
-                Submit Result
-              </button>
+
+              <AlertDialog.Root>
+                <AlertDialog.Trigger asChild>
+                  <button className="mt-3 bg-teal-500 text-white px-4 py-2 rounded-lg">
+                    Submit result
+                  </button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Portal>
+                  <AlertDialog.Overlay className="bg-black opacity-50 data-[state=open]:animate-overlayShow fixed inset-0" />
+                  <AlertDialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-slate-700 p-[25px] ">
+                    <AlertDialog.Title className=" m-0 text-[17px] font-medium">
+                      Are you sure?
+                    </AlertDialog.Title>
+                    <AlertDialog.Description className="mt-4 mb-5 text-[15px] leading-normal">
+                      Your score will be saved in PoolPoint
+                    </AlertDialog.Description>
+                    <div className="flex justify-end gap-[25px] items-center">
+                      <AlertDialog.Cancel asChild>
+                        <button className=" hover:bg-slate-600 inline-flex h-[33px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+                          Cancel
+                        </button>
+                      </AlertDialog.Cancel>
+                      <AlertDialog.Action asChild>
+                        <button
+                          onClick={submitResult}
+                          className=" bg-teal-500 hover:bg-teal-600 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px]"
+                        >
+                          Submit
+                        </button>
+                      </AlertDialog.Action>
+                    </div>
+                  </AlertDialog.Content>
+                </AlertDialog.Portal>
+              </AlertDialog.Root>
             </div>
           </div>
         )}

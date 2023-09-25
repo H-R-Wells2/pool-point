a53ca3dd5db60f2a7f0dd94df08860f5667cbc23
h-r-wells2 <hrwells10246@gmail.com>
2023-09-26 02:14:09 +0530

    removed hardcore values for dates

--- components/DateFilter.tsx
+++ components/DateFilter.tsx
@@ -7,15 +7,14 @@ import { MdOutlineArrowDropDownCircle } from "react-icons/md";
 import type { ChangeEvent } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-const options = ["2023-09-23", "2023-09-24"];
-
 type Props = {
   uniqueDates: string[];
-  // handleDateClick: (selectedDate: string) => void;
   selected: string;
 };
 
 const DateFilter = ({ uniqueDates, selected }: Props) => {
+  const options = uniqueDates;
+
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -40,18 +39,6 @@ const DateFilter = ({ uniqueDates, selected }: Props) => {
   // console.log(uniqueDates);
   const [dates, setDates] = useState(uniqueDates);
 
-  const [visibility, setVisibility] = useState("hidden");
-  const [iconRotate, setIconRotate] = useState("");
-  const toggleVisibility = () => {
-    if (visibility === "hidden") {
-      setVisibility("flex");
-      setIconRotate("-rotate-180");
-    } else {
-      setVisibility("hidden");
-      setIconRotate("");
-    }
-  };
-
   return (
     <div className="mx-6 flex flex-col justify-end">
       <select
@@ -59,7 +46,12 @@ const DateFilter = ({ uniqueDates, selected }: Props) => {
         onChange={onSelect}
         className="px-4 py-1 mb-3 md:w-52 w-40 self-end justify-center border-2 border-teal-300 rounded-lg flex items-center gap-1 bg-transparent outline-none"
       >
-      <option value="" className={`border-teal-300 border p-3 rounded-lg gap-2 absolute top-32 bg-gray-800 transition-all duration-300 ease-in-out animate-dropdown md:w-52 w-40`}>None</option>
+        <option
+          value=""
+          className={`border-teal-300 border p-3 rounded-lg gap-2 absolute top-32 bg-gray-800 transition-all duration-300 ease-in-out animate-dropdown md:w-52 w-40`}
+        >
+          None
+        </option>
         {options.map((opt) => (
           <option
             key={opt}
